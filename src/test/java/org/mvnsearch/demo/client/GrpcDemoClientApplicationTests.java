122608a3f5a763e59477954c6db91d7c55a639be
linux_china <linux_china@hotmail.com>
2017-02-04 15:25:11 +0800

    gRPC client test

---
+++ src/test/java/org/mvnsearch/demo/client/GrpcDemoClientApplicationTests.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.demo.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mvnsearch.service.GreeterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrpcDemoClientApplicationTests {
+
+    @Autowired
+    private GreeterService greeterService;
+	@Test
+	public void testSayHello() {
+        System.out.println(greeterService.sayHello("Jacky"));
+    }
+
+}
