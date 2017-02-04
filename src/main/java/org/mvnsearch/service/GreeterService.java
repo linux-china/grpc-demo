74fa21c215c838b984a9137ccc3bab5712fa28a1
linux_china <linux_china@hotmail.com>
2017-02-04 15:06:38 +0800

    greeter service

---
+++ src/main/java/org/mvnsearch/service/GreeterService.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.service;
+
+/**
+ * greeter service
+ *
+ * @author linux_china
+ */
+public interface GreeterService {
+
+    /**
+     * say hello
+     *
+     * @param name name
+     * @return message
+     */
+    public String sayHello(String name);
+}
