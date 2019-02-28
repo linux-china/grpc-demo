2b1571d14b686031545b8dbbdfb61a759a509ef6
linux_china <libing.chen@gmail.com>
2019-02-28 15:38:10 -0800

    update to junit 5

--- src/test/java/org/mvnsearch/demo/client/GrpcDemoClientApplicationTests.java
+++ src/test/java/org/mvnsearch/demo/client/GrpcDemoClientApplicationTests.java
@@ -1,13 +1,10 @@
 package org.mvnsearch.demo.client;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.mvnsearch.service.GreeterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GrpcDemoClientApplicationTests {
 
